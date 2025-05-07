fe7bf281d73e31d5b5de09c14ce43cb199fd1278
luipe-dev <134946592+luipe-dev@users.noreply.github.com>
2025-05-06 22:18:30 -0700

    limit fps

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 import pygame
-
 from constants import *
 
 def main():
-    pygame.init
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, "black")
+     
+        screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
