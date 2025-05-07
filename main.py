087bfb5126feb0b646f67238310e69d8496ccdeb
luipe-dev <134946592+luipe-dev@users.noreply.github.com>
2025-05-07 11:08:56 -0700

    add player

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    # Initiate Player Object
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y, PLAYER_RADIUS)
+
     dt = 0
 
+    # Game Loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-     
-        screen.fill("black")
-        pygame.display.flip()
-        dt = clock.tick(60) / 1000
+
+        screen.fill("black") # generate black screen
+        player.draw(screen) # render the player on the screen
+        pygame.display.flip() #flip screen
+
+        dt = clock.tick(60) / 1000 #limit framerate to 60 FPS
 
 
 if __name__ == "__main__":
