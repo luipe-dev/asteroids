75c9596cc5787728fc2e35b26448a64f0f004494
luipe-dev <134946592+luipe-dev@users.noreply.github.com>
2025-05-07 21:28:01 -0700

    split asts

--- asteroid.py
+++ asteroid.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 from circleshape import CircleShape
 from constants import *
 
@@ -12,3 +13,18 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += (self.velocity * dt)
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        random_angle = random.uniform(20, 50)
+        velo_1 = self.velocity.rotate(random_angle)*1.2
+        velo_2 = self.velocity.rotate(-random_angle)*1.2
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        asteroid1 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid2 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid1.velocity = velo_1
+        asteroid2.velocity = velo_2
+            
+        
--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def main():
             for shot in shots:
                 if asteroid.collision(shot):
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
         screen.fill("black") # generate black screen
         for obj in drawable:
             obj.draw(screen) # render the player on the screen
